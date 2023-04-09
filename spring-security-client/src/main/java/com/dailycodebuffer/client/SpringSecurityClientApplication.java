0a0412912f10b592f1b00cbb69dc9e78b953c51c
Nizar <nizar.al.nasraoui@gmail.com>
2023-04-09 17:38:21 +0200

    settings fix

--- spring-security-client/src/main/java/com/dailycodebuffer/client/SpringSecurityClientApplication.java
+++ spring-security-client/src/main/java/com/dailycodebuffer/client/SpringSecurityClientApplication.java
@@ -2,6 +2,7 @@ package com.dailycodebuffer.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringSecurityClientApplication {
